a833221531954702105ea42a88c0e3d0e71cb563
yanghoon <ssang276@hanmail.net>
2018-09-12 13:35:36 +0900

    Update restore-jenkins.sh

--- resources/restore-jenkins.sh
+++ resources/restore-jenkins.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-RELEASE=ci
+RELEASE=zcp-jenkins
 POD=$(kubectl get pods --no-headers=true -o custom-columns=:.metadata.name | grep -- $RELEASE-jenkins)
 
 ARCHIVE=$(ls -t jenkins-home-*.zip | head -n1)
@@ -11,16 +11,16 @@ kubectl exec -it $POD -- bash -c "ls -l /tmp"
 kubectl exec -it $POD -- bash -c "cd /tmp && mkdir jenkins-home && unzip -o $ARCHIVE -d jenkins-home"
 
 # Restore Jenkins Home
-kubectl exec -it $GIT_POD -- bash -c "cd /tmp/jenkins-home && cp data/conf/app.ini /data/gitea/conf/"
-kubectl exec -it $GIT_POD -- bash -c "cd /tmp/jenkins-home && unzip gitea-repo.zip -d /data/git"
-kubectl exec -it $GIT_POD -- bash -c "cd /tmp/jenkins-home && cp data/attachments /data/gitea/"
+#kubectl exec -it $GIT_POD -- bash -c "cd /tmp/jenkins-home && cp data/conf/app.ini /data/gitea/conf/"
+#kubectl exec -it $GIT_POD -- bash -c "cd /tmp/jenkins-home && unzip gitea-repo.zip -d /data/git"
+#kubectl exec -it $GIT_POD -- bash -c "cd /tmp/jenkins-home && cp data/attachments /data/gitea/"
 
 # Restore DB_PODB
 # http://pgclks.tistory.com/437
-unzip -o $ARCHIVE gitea-db.sql
-kubectl cp gitea-db.sql $DB_POD:/tmp/gitea-db.sql
-kubectl exec -it $DB_POD -- bash -c "psql gitea < /tmp/gitea-db.sql"
+#unzip -o $ARCHIVE gitea-db.sql
+#kubectl cp gitea-db.sql $DB_POD:/tmp/gitea-db.sql
+#kubectl exec -it $DB_POD -- bash -c "psql gitea < /tmp/gitea-db.sql"
 
 # Restart Service
-kubectl scale --replicas=0 deploy/git-gitea
-kubectl scale --replicas=1 deploy/git-gitea
+#kubectl scale --replicas=0 deploy/git-gitea
+#kubectl scale --replicas=1 deploy/git-gitea
