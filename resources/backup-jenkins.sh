84ce753f460a553b78c0f9e239d7cd3e5d201ed3
a young adelina Lee <lay126@gmail.com>
2019-05-30 17:22:54 +0900

    Update backup-jenkins.sh

--- resources/backup-jenkins.sh
+++ resources/backup-jenkins.sh
@@ -19,29 +19,17 @@ if [ -z "$POD" ]; then
 fi
 
 echo -e "\n\n+ Copy... [pod/$POD -> $BACKUP]"
-# https://github.com/kubernetes/charts/blob/master/stable/jenkins/templates/jenkins-master-deployment.yaml#L64
-#if [ -z $(which rsync) ]; then
-#	echo -e '\n\n+ Install rsync'
-#	apk update
-#	apk add rsync
-#fi
-#time rsync -av -e './rsh.sh' --blocking-io $POD:/var/jenkins_home $BACKUP
-
-echo -e '\n\n+ jekins_home copy…’
 time kubectl exec -it $POD mkdir appdata
 time kubectl exec -it $POD -- cp -rf /var/jenkins_home /appdata/$BACKUP_NAME
 
 echo -e '\n\n+ Compress...'
 time kubectl exec -it $POD -- tar -zcvf /appdata/$BACKUP_NAME.tgz /appdata/$BACKUP_NAME
 time kubectl exec -it $POD -- rm -rf appdata/$BACKUP_NAME
-#time tar -zcvf $BACKUP.tgz $BACKUP > $BACKUP.log
-#rm -rf $BACKUP
+
 
 time kubectl cp $POD:/appdata/$BACKUP_NAME.tgz $BACKUP
 
-#time kubectl cp $POD:/var/jenkins_home $BACKUP
 ls -l $BACKUP
-
 ls -l $BACKUP_DIR
 
 # Trigger s3 upload
