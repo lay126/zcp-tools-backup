97de620663c48eb226af29fc6c9ea0b0cad035ca
a young adelina Lee <lay126@gmail.com>
2019-06-10 11:42:08 +0900

    Update upload-to-s3.sh

--- resources/upload-to-s3.sh
+++ resources/upload-to-s3.sh
@@ -7,7 +7,7 @@ BIN="$( cd "$( dirname "$0" )" && pwd )"   # https://stackoverflow.com/a/2043474
 #cat $BIN/setenv      # for logging
 
 # Wait trigger of backup script
-echo -e "\n\n+ Wait archiving..."
+echo -e "\n\n+ [backup] Wait archiving..."
 while [ ! -f $S3_TRIGGER ]; do sleep 2s; done
 if [ 'ERROR' = "$(cat $S3_TRIGGER)" ]; then
 	echo -e '\n\n+ ERROR :: Fail when backup stage.'
@@ -15,8 +15,9 @@ if [ 'ERROR' = "$(cat $S3_TRIGGER)" ]; then
 fi
 #rm $S3_TRIGGER
 
-echo -e "\n\n+ Listing backups..."
-aws --endpoint-url $S3_ENDPOINT s3 ls $S3_BUCKET
+#echo -e "\n\n+ [backup] Listing backups..."
+#aws --endpoint-url $S3_ENDPOINT s3 ls $S3_BUCKET
 
-echo -e "\n\n+ Upload backup files..."
+echo -e "\n\n+ [backup] start Upload backup files..."
 time aws --endpoint-url $S3_ENDPOINT s3 cp $BACKUP_DIR s3://$S3_BUCKET --recursive
+echo -e "\n\n+ [backup] end Upload backup files..."
